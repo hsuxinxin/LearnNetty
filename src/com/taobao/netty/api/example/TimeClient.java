c255a095bb788973f6d925529899d45f94709918
xinxin <xxxnju@163.com>
2014-10-24 17:03:58 +0800

    push

---
+++ src/com/taobao/netty/api/example/TimeClient.java
@@ -0,0 +1,46 @@
+package com.taobao.netty.api.example;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class TimeClient {
+
+	public void connect(int port, String host) throws   Exception{
+		EventLoopGroup group = new NioEventLoopGroup();
+		try{
+			Bootstrap b = new Bootstrap();
+			b.group(group)
+			.channel(NioSocketChannel.class)
+			.option(ChannelOption.TCP_NODELAY, true)
+			.handler(new ChannelInitializer<SocketChannel>() {
+
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					// TODO Auto-generated method stub
+					ch.pipeline().addLast(new LineBasedFrameDecoder(2014));
+					ch.pipeline().addLast(new StringDecoder());
+					ch.pipeline().addLast(new TimeClientHandler());
+				}
+			});
+			
+			ChannelFuture f = b.connect(host, port).sync();
+			f.channel().closeFuture().sync();			
+		}finally{
+			group.shutdownGracefully();
+		}
+		System.out.println("TimeClient shutdown");
+	}
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		new TimeClient().connect(8080, "127.0.0.1");
+	}
+
+}
